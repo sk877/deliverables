fe93b064f7837f8e99b56bada759b09c57de6e8e
sk877 <55451486+sk877@users.noreply.github.com>
2020-09-24 12:25:24 +0900

    Add files via upload

---
+++ CalcuratorMain.java
@@ -0,0 +1,26 @@
+package jp.co.learning2;
+
+public class CalcuratorMain {
+
+	public static void main(String[] args) {
+		// 変数の宣言
+		String ope1 = "=";
+		String ope2 = "+";
+		String oep3 = "-";
+		String ope4 = "*";
+		String ope5 = "/";
+
+		int int0 = 0;
+		int int1 = 1;
+		int int2 = 2;
+		int int3 = 3;
+		int int4 = 4;
+		int int5 = 5;
+		int int6 = 6;
+		int int7 = 7;
+		int int8 = 8;
+		int int9 = 9;
+
+	}
+
+}
